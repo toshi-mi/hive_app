a57f42974c42be40a1fff58cb2d457ff52b8c8b9
toshi443 <toshi.443.aki@gmail.com>
2016-05-01 03:58:47 +0000

    add users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+def index
+    @users = User.all
+end
+
+def show
+    @user = User.find(params[:id])
+end
+
+def new
+    @user = User.new
+end
+
+def create
+    user = User.new(user_params)
+    user.save
+    redirect_to users_path
+end
+def edit
+    @user = User.find(params[:id])
+end
+
+def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+    redirect_to user_path
+end
+
+def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
+end
+private
+    def user_params
+        params.require(:user).permit(:name,
+        :email, :profile)
+    end
+
+end
